3a783048610da203455907b38476421b5342783e
Mattia Marchesini <mattia.marchesini1999@gmail.com>
2023-03-17 15:44:22 +0100

    first commit

---
+++ init.sh
@@ -0,0 +1,46 @@
+export DEBEZIUM_VERSION=2.0
+
+#### MYSQL ####
+
+# Start the topology as defined in https://debezium.io/documentation/reference/stable/tutorial.html
+docker-compose -f docker-compose-mysql.yaml up
+
+# Start MySQL connector
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @register-mysql.json
+
+# Consume messages from a Debezium topic
+docker-compose -f docker-compose-mysql.yaml exec kafka /kafka/bin/kafka-console-consumer.sh \
+    --bootstrap-server kafka:9092 \
+    --from-beginning \
+    --property print.key=true \
+    --topic dbserver1.inventory.customers
+
+# Modify records in the database via MySQL client
+docker-compose -f docker-compose-mysql.yaml exec mysql bash -c 'mysql -u $MYSQL_USER -p$MYSQL_PASSWORD inventory'
+
+# Shut down the cluster
+# docker-compose -f docker-compose-mysql.yaml down
+
+#### SQLSERVER ####
+
+# Start the topology as defined in https://debezium.io/documentation/reference/stable/tutorial.html
+docker-compose -f docker-compose-sqlserver.yaml up
+
+# Initialize database and insert test data
+cat debezium-sqlserver-init/inventory.sql | docker-compose -f docker-compose-sqlserver.yaml exec -T sqlserver bash -c '/opt/mssql-tools/bin/sqlcmd -U sa -P $SA_PASSWORD'
+
+# Start SQL Server connector
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @register-sqlserver.json
+
+# Consume messages from a Debezium topic
+docker-compose -f docker-compose-sqlserver.yaml exec kafka /kafka/bin/kafka-console-consumer.sh \
+    --bootstrap-server kafka:9092 \
+    --from-beginning \
+    --property print.key=true \
+    --topic server1.testDB.dbo.customers
+
+# Modify records in the database via SQL Server client (do not forget to add `GO` command to execute the statement)
+docker-compose -f docker-compose-sqlserver.yaml exec sqlserver bash -c '/opt/mssql-tools/bin/sqlcmd -U sa -P $SA_PASSWORD -d testDB'
+
+# Shut down the cluster
+# docker-compose -f docker-compose-sqlserver.yaml down
