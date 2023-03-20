2309e041a800e48ed50d8990de1bf3c5eee44e9f
Mattia Marchesini <mattia.marchesini1999@gmail.com>
2023-03-20 13:41:56 +0100

    ci siamo quasi

--- init.sh
+++ init.sh
@@ -1,46 +1,44 @@
-export DEBEZIUM_VERSION=2.0
+export DEBEZIUM_VERSION='2.0'
+export SQLSERVER_USER='sa'
+export SQLSERVER_PW='Password!'
+export SQLSERVER_PORT='1433'
+export MYSQL_USER=''
+export MYSQL_PW=''
+export MYSQL_PORT='3306'
+export KAFKA_PORT='9092'
+export KAFKA_CONNECT_PORT='8083'
 
-#### MYSQL ####
-
-# Start the topology as defined in https://debezium.io/documentation/reference/stable/tutorial.html
-docker-compose -f docker-compose-mysql.yaml up
+# Start the containers in detached mode
+docker-compose up -d
 
 # Start MySQL connector
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @register-mysql.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" "http://localhost:${KAFKA_CONNECT_PORT}/connectors/" -d @register-mysql.json
 
 # Consume messages from a Debezium topic
-docker-compose -f docker-compose-mysql.yaml exec kafka /kafka/bin/kafka-console-consumer.sh \
-    --bootstrap-server kafka:9092 \
-    --from-beginning \
-    --property print.key=true \
-    --topic dbserver1.inventory.customers
+# docker-compose -f docker-compose-mysql.yaml exec kafka /kafka/bin/kafka-console-consumer.sh \
+#     --bootstrap-server kafka:${KAFKA_PORT} \
+#     --from-beginning \
+#     --property print.key=true \
+#     --topic dbserver1.inventory.customers
 
 # Modify records in the database via MySQL client
-docker-compose -f docker-compose-mysql.yaml exec mysql bash -c 'mysql -u $MYSQL_USER -p$MYSQL_PASSWORD inventory'
-
-# Shut down the cluster
-# docker-compose -f docker-compose-mysql.yaml down
-
-#### SQLSERVER ####
-
-# Start the topology as defined in https://debezium.io/documentation/reference/stable/tutorial.html
-docker-compose -f docker-compose-sqlserver.yaml up
+# docker-compose -f docker-compose-mysql.yaml exec mysql bash -c 'mysql -u $MYSQL_USER -p$MYSQL_PASSWORD inventory'
 
 # Initialize database and insert test data
-cat debezium-sqlserver-init/inventory.sql | docker-compose -f docker-compose-sqlserver.yaml exec -T sqlserver bash -c '/opt/mssql-tools/bin/sqlcmd -U sa -P $SA_PASSWORD'
+cat debezium-sqlserver-init/inventory.sql | docker-compose -f prova.yaml exec -T sqlserver bash -c '/opt/mssql-tools/bin/sqlcmd -U sa -P $SA_PASSWORD'
 
 # Start SQL Server connector
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @register-sqlserver.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:${KAFKA_CONNECT_PORT}/connectors/ -d @register-sqlserver.json
 
 # Consume messages from a Debezium topic
-docker-compose -f docker-compose-sqlserver.yaml exec kafka /kafka/bin/kafka-console-consumer.sh \
-    --bootstrap-server kafka:9092 \
+docker-compose exec kafka /kafka/bin/kafka-console-consumer.sh \
+    --bootstrap-server "kafka:${KAFKA_PORT}" \
     --from-beginning \
     --property print.key=true \
     --topic server1.testDB.dbo.customers
 
 # Modify records in the database via SQL Server client (do not forget to add `GO` command to execute the statement)
-docker-compose -f docker-compose-sqlserver.yaml exec sqlserver bash -c '/opt/mssql-tools/bin/sqlcmd -U sa -P $SA_PASSWORD -d testDB'
+docker-compose exec sqlserver bash -c '/opt/mssql-tools/bin/sqlcmd -U sa -P $SA_PASSWORD -d testDB'
 
 # Shut down the cluster
-# docker-compose -f docker-compose-sqlserver.yaml down
+# docker-compose down
--- kafka-connect-with-jdbc.Dockerfile
+++ kafka-connect-with-jdbc.Dockerfile
@@ -3,12 +3,12 @@ FROM quay.io/debezium/connect:$DEBEZIUM_VERSION
 ENV KAFKA_CONNECT_JDBC_DIR=$KAFKA_CONNECT_PLUGINS_DIR/kafka-connect-jdbc
 
 # These should point to the driver version to be used
-ENV MAVEN_DEP_DESTINATION=$KAFKA_HOME/libs \
-    ORACLE_JDBC_REPO="com/oracle/database/jdbc" \
-    ORACLE_JDBC_GROUP="ojdbc8" \
-    ORACLE_JDBC_VERSION="21.6.0.0" \
-    ORACLE_JDBC_MD5=312e6f4ec9932bbf74a4461669970c4b
+ENV MAVEN_DEP_DESTINATION=$KAFKA_CONNECT_JDBC_DIR \
+    REPO="confluent" \
+    PACKAGE="kafka-connect-jdbc" \
+    VERSION="5.4.0" \
+    MD5_CHECKSUM="70B7ABBF19AA4CF57DCAF4B0B075D937"
 
-RUN docker-maven-download central "$ORACLE_JDBC_REPO" "$ORACLE_JDBC_GROUP" "$ORACLE_JDBC_VERSION" "$ORACLE_JDBC_MD5"
+RUN mkdir ${KAFKA_CONNECT_JDBC_DIR} && docker-maven-download "${REPO}" "$PACKAGE" "$VERSION" "$MD5_CHECKSUM"
 
 USER kafka
